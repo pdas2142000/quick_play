27aeeb9161599214c61b9b5f8830047d5c2d86c1
pdas2142000 <pdas2142000@gmail.com>
2024-11-04 20:42:08 +0530

    feat: 🎸 add Libraries

---
+++ src/utils/styles/index.jsx
@@ -0,0 +1,10 @@
+export const Fonts = {
+    Font_100: 'Urbanist-Thin',
+    Font_200: 'Urbanist-ExtraLight',
+    Font_300: 'Urbanist-Light',
+    Font_400: 'Urbanist-Regular',
+    Font_500: "Urbanist-Medium",
+    Font_600: 'Urbanist-SemiBold',
+    Font_700: 'Urbanist-Bold',
+    Font_800: 'Urbanist-Black',
+}
